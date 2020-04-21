995af9a392f0297954ed157cb5a7010204b0bd78
Tatsuya Kitajima <dogfoodinglab@gmail.com>
2020-04-21 21:04:50 +0900

    update

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
